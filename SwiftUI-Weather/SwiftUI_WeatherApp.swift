38cbb21d2aec7c0cd3e9d7c9c46a1b51f0d0044c
William Wang <wmwang52@gmail.com>
2022-09-29 12:57:58 -0400

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by William Wang on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
