33c2b477287c618846ed993981d0a8edb691152f
William Wang <wmwang52@gmail.com>
2022-09-29 17:12:51 -0400

    Made the Weather Icons Random and Fixed the Nighttime Icons. Also decreases temp during nighttime.

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -11,48 +11,40 @@ struct ContentView: View {
     
     @State private var isNight = false
     
+    var Day :[String] = ["Mon","Tues", "Wed", "Thurs", "Fri"]
+
+    var Temperature :[Int] = [78,62,73,93,85]
+    
     var body: some View {
         ZStack {
             BackgroundView(isNight: $isNight)
             
             VStack{
-                CityName()
+                CityName(CityName: "Cupertino CA")
                     
-                MainWeatherView(imageName: "cloud.sun.fill")
+                MainWeatherView(isNight: $isNight)
+                
+                
                 
                 HStack(spacing:20){
-                    WeatherDayView(weekday: "Mon",
-                                   photo: "cloud.fill",
-                                   temp: "72°")
-                    
-                    WeatherDayView(weekday: "Mon",
-                                   photo: "cloud.fill",
-                                   temp: "72°")
-                    
-                    WeatherDayView(weekday: "Mon",
-                                   photo: "cloud.fill",
-                                   temp: "72°")
-                    
-                    WeatherDayView(weekday: "Mon",
-                                   photo: "cloud.fill",
-                                   temp: "72°")
-                    
-                    WeatherDayView(weekday: "Mon",
-                                   photo: "cloud.fill",
-                                   temp: "72°")
+                    ForEach((0...4),
+                            id: \.self) {
+                        WeatherDayView(isNight: $isNight,
+                                       weekday: Day[$0] ,
+                                       temp: isNight ? Temperature[$0] - 20 : Temperature[$0])
+                       }
                 }
                 Spacer()
                 
                 Button{
                     isNight.toggle()
                 } label:{
-                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                    WeatherButton(title: "Change Day Time",
+                                  textColor: .blue,
+                                  backgroundColor: .white)
                 }
-                
                 Spacer()
             }
-            
-            
         }
     }
 }
@@ -63,32 +55,31 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-
 struct WeatherDayView: View {
-    
+    @Binding var isNight: Bool
     var weekday: String
-    var photo: String
-    var temp: String
-    
+    let randomInt = Int.random(in: 0...4)
+    var Weather :[String] = ["sun.max.fill", "wind", "snowflake", "tornado", "cloud.sun.rain.fill"]
+    var temp: Int
     var body: some View {
-        
         VStack{
             Text(weekday)
-                .font(.system(size: 16, weight: .medium, design: .default))
+                .font(.system(size: 16,
+                              weight: .medium,
+                              design: .default))
                 .foregroundColor(.white)
             
-            Image(systemName:photo)
+            Image(systemName:isNight ? "moon.stars.fill" : Weather[randomInt])
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width:40,
                        height:40)
-            Text(temp)
-                .font(.system(size:28,weight:.medium))
+            Text("\(temp)°")
+                .font(.system(size:28,
+                              weight:.medium))
                 .foregroundColor(.white)
-            
-            
-             
+
         }
     }
 }
@@ -97,9 +88,8 @@ struct BackgroundView: View {
     @Binding var isNight: Bool
     
     var body: some View {
-        
-        
-        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue, isNight ? .gray : Color("lightBlue")]),
+        LinearGradient(gradient: Gradient(colors: [isNight ? .black : .blue,
+                                                   isNight ? .gray : Color("lightBlue")]),
                        startPoint:.topLeading,
                        endPoint:.bottomTrailing)
         .edgesIgnoringSafeArea(.all)
@@ -107,34 +97,33 @@ struct BackgroundView: View {
 }
 
 struct MainWeatherView: View{
-    var imageName:String
-    
+
+    @Binding var isNight:Bool
     var body: some View{
         VStack(spacing:10){
-            Image(systemName:imageName)
+            Image(systemName:isNight ? "moon.stars.fill" : "cloud.sun.fill" )
                 .renderingMode(.original)
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width:180,
                        height:180)
-            
             Text("76°")
-                .font(.system(size:70,weight:.medium))
+                .font(.system(size:70,
+                              weight:.medium))
                 .foregroundColor(.white)
         }
-        .padding(.bottom,40)
+        .padding(.bottom, 40)
     }
 }
 
 struct CityName: View {
+    var CityName:String
     var body: some View {
-        Text("Cupertino CA")
+        Text(CityName)
             .font(.system(size:32,
                           weight:.medium,
                           design:.default))
             .foregroundColor(.white)
             .padding()
-        
     }
-    
 }
